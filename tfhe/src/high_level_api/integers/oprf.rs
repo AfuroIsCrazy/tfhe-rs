641ee0f534ee0ddedf6bcfebcfd79a72c419e347
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2025-12-01 17:53:51 +0100

    f

--- tfhe/src/high_level_api/integers/oprf.rs
+++ tfhe/src/high_level_api/integers/oprf.rs
@@ -4,6 +4,7 @@ use crate::high_level_api::keys::InternalServerKey;
 use crate::high_level_api::re_randomization::ReRandomizationMetadata;
 #[cfg(feature = "gpu")]
 use crate::integer::gpu::ciphertext::{CudaSignedRadixCiphertext, CudaUnsignedRadixCiphertext};
+use crate::shortint::MessageModulus;
 use crate::{FheInt, Seed};
 
 impl<Id: FheUintId> FheUint<Id> {
@@ -92,7 +93,7 @@ impl<Id: FheUintId> FheUint<Id> {
             }
         })
     }
-    /// Generates an encrypted `num_block` blocks unsigned integer
+    /// Generates an encrypted unsigned integer
     /// taken uniformly in `[0, 2^random_bits_count[` using the given seed.
     /// The encrypted value is oblivious to the server.
     /// It can be useful to make server random generation deterministic.
@@ -150,6 +151,72 @@ impl<Id: FheUintId> FheUint<Id> {
             }
         })
     }
+
+    /// Generates an encrypted unsigned integer
+    /// taken almost uniformly in `[0, excluded_upper_bound[` using the given seed.
+    /// The encrypted value is oblivious to the server.
+    /// It can be useful to make server random generation deterministic.
+    ///
+    /// The norm-1 distance (defined as ∆(, ) := 1/2 Sum(ω∈Ω) |P(ω) − Q(ω)|) between the actual distribution and the target uniform distribution is below the `max_distance` argument.
+    ///
+    /// A safe value for `max_distance` is `2^-128`
+    ///
+    /// Higher values allow better performance but must be considered carefully in the context of their target application
+    /// as it may have serious unintended consequences.
+    ///
+    /// ```rust
+    /// use tfhe::prelude::FheDecrypt;
+    /// use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint8, Seed};
+    ///
+    /// let config = ConfigBuilder::default().build();
+    /// let (client_key, server_key) = generate_keys(config);
+    ///
+    /// set_server_key(server_key);
+    ///
+    /// let excluded_upper_bound = 3;
+    ///
+    /// let ct_res = FheUint8::generate_oblivious_pseudo_random_custom_range(Seed(0), excluded_upper_bound, 2_f64.powi(-128));
+    ///
+    /// let dec_result: u16 = ct_res.decrypt(&client_key);
+    /// assert!(dec_result < excluded_upper_bound as u16);
+    /// ```
+    pub fn generate_oblivious_pseudo_random_custom_range(
+        seed: Seed,
+        excluded_upper_bound: u64,
+        max_distance: f64,
+    ) -> Self {
+        global_state::with_internal_keys(|key| match key {
+            InternalServerKey::Cpu(key) => {
+                let num_input_random_bits = num_input_random_bits_for_bias(
+                    excluded_upper_bound,
+                    max_distance,
+                    key.message_modulus(),
+                );
+
+                let num_blocks_output = Id::num_blocks(key.message_modulus()) as u64;
+
+                let ct = key
+                    .pbs_key()
+                    .par_generate_oblivious_pseudo_random_unsigned_custom_range(
+                        seed,
+                        num_input_random_bits,
+                        excluded_upper_bound,
+                        num_blocks_output,
+                    );
+
+                Self::new(ct, key.tag.clone(), ReRandomizationMetadata::default())
+            }
+            #[cfg(feature = "gpu")]
+            InternalServerKey::Cuda(cuda_key) => {
+                panic!("Gpu does not support this operation yet.")
+            }
+            #[cfg(feature = "hpu")]
+            InternalServerKey::Hpu(_device) => {
+                panic!("Hpu does not support this operation yet.")
+            }
+        })
+    }
+
     #[cfg(feature = "gpu")]
     /// Returns the amount of memory required to execute generate_oblivious_pseudo_random_bounded
     ///
@@ -183,6 +250,29 @@ impl<Id: FheUintId> FheUint<Id> {
             }
         })
     }
+}
+
+fn num_input_random_bits_for_bias(
+    excluded_upper_bound: u64,
+    max_distance: f64,
+    message_modulus: MessageModulus,
+) -> u64 {
+    let mut random_block_count = 1;
+
+    let random_block_count = loop {
+        let remainder =
+            ((1_u128 << (2 * random_block_count)) % excluded_upper_bound as u128) as f64;
+
+        let bias = remainder * (excluded_upper_bound as f64 - remainder)
+            / (4_f64.powi(random_block_count as i32) * excluded_upper_bound as f64);
+        if bias < max_distance {
+            break random_block_count;
+        }
+
+        random_block_count += 1;
+    };
+
+    random_block_count * message_modulus.0 as u64
 }
 
 impl<Id: FheIntId> FheInt<Id> {
